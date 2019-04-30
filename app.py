a5fa1597f3cda242fd74b7dbbfbc6ac0d52d0b62
FedericoCarboni <carboni.federico24@gmail.com>
2019-04-30 15:33:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import flask
 app = flask.Flask(__name__)
 
 
-@app.route('/<string:lang>')
+@app.route('/<string:lang>/')
 def index(lang):
     return flask.render_template(f'index.{lang}.html')
 
 
-@app.route('/<string:lang>/quiz', methods=['GET', 'POST'])
+@app.route('/<string:lang>/quiz/', methods=['GET', 'POST'])
 def quiz(lang):
     print(flask.request.method)
     return flask.render_template(f'quiz.{lang}.html')
