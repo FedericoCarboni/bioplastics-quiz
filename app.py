26ddb4c1b11e622152484a4abda1e079a639ca44
FedericoCarboni <carboni.federico24@gmail.com>
2019-05-03 22:39:20 +0200

    Changes

--- app.py
+++ app.py
@@ -1,49 +1,86 @@
-import flask
+import datetime
 import json
-import os
+
+import flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = flask.Flask(__name__)
-langs = ('en-EU', 'it-IT')
-results = []
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///results.db'
+
+db = SQLAlchemy(app)
 
 with app.open_resource('static/dat/quiz.json') as f:
-  quiz_json = json.load(f)
+    quiz_json = json.load(f)
+
+class Result(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    score = db.Column(db.Float, nullable=False)
+    time = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f"Result(score={self.score}, time='{self.time}')"
+    
+    def __radd__(self, other):
+        return self.score + other
+
+
+langs = ('en', 'it')
 
 
 @app.route('/')
 def index_redirect():
-  return flask.redirect('/en-EU/')
+    return flask.redirect('/' + langs[0])
 
 
 @app.route('/<string:lang>/')
-def index(lang):
-  if not lang in langs:
-    return flask.abort(404)
-  return flask.render_template('index.' + lang + '.html')
+def index(lang: str):
+    if lang not in langs:
+        return flask.abort(404)
+    return flask.render_template(f'{lang}/index.html')
 
 
 @app.route('/<string:lang>/quiz/')
-def quiz(lang):
-  if quiz_json.get(lang) is None:
-    return flask.abort(404)
-  _quiz = quiz_json.get(lang)
-  return flask.render_template('quiz.html', quiz=_quiz)
+def quiz(lang: str):
+    if lang not in langs:
+        return flask.abort(404)
+    return flask.render_template('quiz.html', quiz=quiz_json[lang])
 
 
-@app.route('/<string:lang>/quiz/result', methods=['GET', 'POST'])
-def result(lang):
-  if not lang in langs:
-    return flask.abort(404)
-  if flask.request.method == 'POST':
-    average = sum(results) / len(results) if len(results) != 0 else 0
-    result = flask.request.form.get('score')
-    result = result if result is not None else flask.abort(400)
-    result = float(result) if result.isdigit() else flask.abort(400)
-    results.append(result)
-    return flask.render_template('result.' + lang + '.html', result=result, average=average)
-  else:
-    return flask.redirect('/' + lang + '/quiz/')
+@app.route('/<string:lang>/quiz/result/', methods=['GET', 'POST'])
+def result(lang: str):
+    if lang not in langs:
+        return flask.abort(404)
+    if flask.request.method == 'POST':
+        results = Result.query.all()
+        average = sum(results) / len(results) if len(results) != 0 else 0
+        score = flask.request.form.get('score')
+        score = score if score is not None else flask.abort(400)
+        score = float(score) if score.isdigit() else flask.abort(400)
+        db.session.add(Result(score=score, time=datetime.datetime.utcnow().strftime('%d/%m/%Y')))
+        db.session.commit()
+        tmp = []
+        chart_data = {}
+        chart_data['labels'] = []
+        chart_data['datasets'] = []
+        for result in results:
+            if result.time not in chart_data['labels']:
+                delta = datetime.datetime.utcnow() - datetime.datetime.strptime(result.time, '%d/%m/%Y') 
+                if delta < datetime.timedelta(days=120):
+                    chart_data['labels'].append(result.time)
+        data = []
+        for time in chart_data['labels']:
+            temp = []
+            for item in results:
+                if item.time == time:
+                    temp.append(item)
+            avg = sum(temp) / len(temp) if len(temp) != 0 else -1
+            if avg != -1:
+                data.append(avg)
+        chart_data['datasets'].append({'label': 'Average per day' if lang == 'en' else 'Media giornaliera', 'data': data, 'borderColor': '#007bff'})
 
-
-if __name__ == '__main__':
-  app.run(debug=True, port=80)
+        return flask.render_template(f'{lang}/result.html', score=score, average=average, chart_data=chart_data)
+    else:
+        return flask.redirect(f'/{lang}/quiz/')
